0a481865ef53f264147959317cf2cbab90a4d0e9
Twinkle priya <102142631+Twinkle-priya@users.noreply.github.com>
2023-10-17 18:04:04 +0530

    Add files via upload
    hii

---
+++ hello.js
@@ -0,0 +1,60 @@
+
+//console.log("HELLO WORLD!!");
+
+// LOOPS
+//1). FOR LOOP 
+/*for( var i=0; i<5; i++){
+    let name= "john Doe";
+    console.log("Hi My Name is :- "+ name);
+}*/
+
+// if else loop 
+/*let age = 18;
+if(age>18){
+console.log("  You are Adult");
+}
+else{
+    console.log("You are Teenager");
+}*/
+
+//STRING concat
+/* str1 = 'Hello'
+str2 = ' world'
+str3 = str1.concat(str2)
+console.log(str3);*/
+
+//INDEX OF STRING
+/*str1 = ' Ram is a good boy'
+substr = 'good'
+ result = str1.indexOf(substr)
+console.log(result);*/
+
+//Switch case
+let day;
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case  6:
+    day = "Saturday";
+}
+console.log(day);
+
+
+
+
